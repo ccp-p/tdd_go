7e5b5133de0bf8120a07e1703531e58ca798ca89
chenchengpeng <chenchengpeng@richinfo.cn>
2024-10-23 18:24:59 +0800

    time

--- poker/cli.go
+++ poker/cli.go
@@ -4,22 +4,33 @@ import (
 	"bufio"
 	"io"
 	"strings"
+	"time"
 )
 
 type CLI struct {
 	playerStore PlayerStore
 	in           *bufio.Scanner
+    alerter     BlindAlerter
 }
 
 func (cli *CLI) PlayPoker() {
+    blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+	for _, blind := range blinds {
+		cli.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime = blindTime + 10*time.Minute
+	}
+
     userInput := cli.readLine()
+    
     cli.playerStore.RecordWin(extractWinner(userInput))
 }
 
-func NewCLI(store PlayerStore, in io.Reader) *CLI {
+func NewCLI(store PlayerStore, in io.Reader,alerter BlindAlerter) *CLI {
     return &CLI{
         playerStore: store,
         in:          bufio.NewScanner(in),
+        alerter:alerter,
     }
 }
 func extractWinner(userInput string) string {
@@ -29,3 +40,4 @@ func (cli *CLI) readLine() string {
     cli.in.Scan()
     return cli.in.Text()
 }
+
